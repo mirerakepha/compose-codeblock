911590a1350d1f80680336383338c6efbc7fae22
mirerakepha <kephamirera16@gmail.com>
2026-03-25 16:17:12 +0300

    added a narrow border around the codeblock

--- codeblock/build.gradle.kts
+++ codeblock/build.gradle.kts
@@ -49,7 +49,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.mirerakepha"
             artifactId = "compose-codeblock"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
